2060be3aa7c18a33c70cd097ccc72ae75d02d292
Bohdane <berlo1113@gmail.com>
2023-04-19 19:24:09 +0300

    modified:   src/App.js 	new file:   src/components/Articles.jsx 	new file:   src/components/BackgroundButton.jsx 	modified:   src/components/Header.jsx 	modified:   src/index.css

--- src/App.js
+++ src/App.js
@@ -4,16 +4,24 @@ import './App.css';
 import Header from './components/Header';
 import PersonalPronouns from './components/PersonalPronouns';
 import Plural from './components/Plural';
+import Articles from './components/Articles';
+import BackgoundButton from './components/BackgroundButton';
 function App() {
   const[selectMenu,setSelectMenu]=useState('PersonalPronouns');
   return (
+    <>
+    <BackgoundButton/>
     <div className='max-w-[1300px] mx-auto border bg-slate-100'>
       <Header selectNow={selectMenu} select={setSelectMenu}/>
       <main className='w-full pb-[40px] px-[10px]' >
         {selectMenu === 'PersonalPronouns' ? <PersonalPronouns/> : null}
         {selectMenu === 'Plural' ? <Plural/> : null}
+        {selectMenu === 'Articles' ? <Articles/> : null}
       </main>
+      
     </div>
+    
+    </>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,13 @@
 const Header = ({selectNow,select}) =>{
     let selectClass = 'bg-green-600';
     return (
-        <header className="w-full h-[80px] bg-indigo-500 rounded-[10px]">
+        <header className="w-full min-h-[80px] bg-indigo-500 rounded-[10px]">
             
-            <nav className="h-full w-full">
-                <ul className="h-full w-full max-w-full border   flex justify-center gap-[20px] items-center flex-grow  text-center text-fuchsia-100  text-[20px]  list-none">
-                    <li className={`after:content-none before:content-none  rounded-xl p-[10px] ${selectNow === 'PersonalPronouns' ? selectClass : null}`}><button onClick={(e)=>select(e.target.value) } value='PersonalPronouns'>Личные местоимения</button></li>
-                    <li className={`after:content-none before:content-none  rounded-xl p-[10px] ${selectNow === 'Plural' ? selectClass : null}`}><button onClick={(e)=>select(e.target.value) } value='Plural'>Множественное число</button></li>
+            <nav className="h-full w-full py-[5px]">
+                <ul className=" min-h-[100px] w-full max-w-full   flex justify-center gap-[20px] items-center flex-wrap  text-center text-fuchsia-100  text-[20px]  list-none">
+                    <li className={`after:content-none before:content-none min-w-[200px]  rounded-xl p-[10px] ${selectNow === 'PersonalPronouns' ? selectClass : null}`}><button onClick={(e)=>select(e.target.value) } value='PersonalPronouns'>Личные местоимения</button></li>
+                    <li className={`after:content-none before:content-none min-w-[200px]  rounded-xl p-[10px] ${selectNow === 'Plural' ? selectClass : null}`}><button onClick={(e)=>select(e.target.value) } value='Plural'>Множественное число</button></li>
+                    <li className={`after:content-none before:content-none min-w-[200px]  rounded-xl p-[10px] ${selectNow === 'Articles' ? selectClass : null}`}><button onClick={(e)=>select(e.target.value) } value='Articles'>Артикли</button></li>
                 </ul>
             </nav>
         </header>
